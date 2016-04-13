725cfa240453c74899c3d2003fb34866c54ba6ae
ridho <ridho_maulana@outlook.com>
2016-04-13 18:39:59 +0700

    New 2st

---
+++ app/src/main/java/id/blastering/mylibrary/dialogblastering/Splashscreen.java
@@ -0,0 +1,18 @@
+package id.blastering.mylibrary.dialogblastering;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import id.blastering.mylibrary.R;
+
+/**
+ * Created by macbook on 4/13/16.
+ */
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
